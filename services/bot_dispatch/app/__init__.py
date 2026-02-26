f762f28fe09dd90c13cbf18709d3e1f7a703e338
Aleksey Pavlov <sakhawood@gmail.com>
2026-02-26 16:13:57 +0900

    init

--- services/bot_dispatch/app/__init__.py
+++ services/bot_dispatch/app/__init__.py
@@ -1,8 +1,1 @@
 print("INIT SHEETS START", flush=True)
-self.events_book = self.client.open("Order_Yakutia.media")
-self.photographers_book = self.client.open("Order_Photographers")
-
-self.sheet_events = self.get_orders_sheet()
-self.sheet_assignments = self.get_assignments_sheet()
-self.sheet_photographers = self.get_photographers_sheet()
-self.sheet_notifications = self.get_notifications_sheet()
