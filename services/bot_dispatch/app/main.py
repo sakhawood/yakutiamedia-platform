7de3ffe18264eb01357626c834c5987d798481f6
Aleksey Pavlov <sakhawood@gmail.com>
2026-02-26 10:56:50 +0900

    полная фиксация main.py

--- services/bot_dispatch/app/main.py
+++ services/bot_dispatch/app/main.py
@@ -1,32 +1,37 @@
 import asyncio
 import logging
 from telegram.ext import ApplicationBuilder
-from .bot_photographers import register_handlers, check_orders
+
 from .config import BOT_TOKEN
+from .bot_photographers import register_handlers, check_orders
+
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
 
+
 async def main():
     print("BOT B STARTING")
 
-    ApplicationBuilder().token(BOT_TOKEN).build()
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
 
+    # регистрация кнопок и обработчиков
     register_handlers(application)
 
-    # Проверка заявок каждые 30 секунд
+    # проверка заказов каждые 30 секунд
     application.job_queue.run_repeating(
         check_orders,
         interval=30,
-        first=10
+        first=5
     )
 
     await application.initialize()
     await application.start()
     await application.updater.start_polling()
     await application.idle()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
