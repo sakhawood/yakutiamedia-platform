04fa730e8d419070da3f480c41b8b2506214abb6
Aleksey Pavlov <sakhawood@gmail.com>
2026-03-11 11:40:49 +0900

    fix: remove google sheets dependency from event id generator

--- core/id_service.py
+++ core/id_service.py
@@ -1,12 +1,8 @@
 import random
 import string
 
-def generate_event_id(sheet, length=5):
+def generate_event_id(length=7):
+
     characters = string.ascii_uppercase + string.digits
 
-    existing_ids = set(sheet.col_values(1))
-
-    while True:
-        event_id = ''.join(random.choices(characters, k=length))
-        if event_id not in existing_ids:
-            return event_id
+    return "".join(random.choices(characters, k=length))
